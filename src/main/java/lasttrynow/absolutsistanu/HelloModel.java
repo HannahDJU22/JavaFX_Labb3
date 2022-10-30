5de9c951e76324d18969fa87bca643582000e1d1
hannah <hannah.degerheim@iths.se>
2022-10-30 15:13:39 +0100

    Initial commit

---
+++ src/main/java/lasttrynow/absolutsistanu/HelloModel.java
@@ -0,0 +1,2 @@
+package lasttrynow.absolutsistanu;public class HelloModel {
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lasttrynow.absolutsistanu {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lasttrynow.absolutsistanu to javafx.fxml;
+    exports lasttrynow.absolutsistanu;
+}
